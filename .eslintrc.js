01c9fb62e3a6d4d680bf54de143cb52508311b99
Erwan. J <41341290+esealcode@users.noreply.github.com>
2018-08-18 23:43:40 +0200

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "modules": true,
+            "experimentalObjectRestSpread": true
+        }
+    },
+    rules: {
+        "no-console": 0
+    }
+};
